a5fd03560580c9cfb71bbafebb1afe5e2998ea90
CharlieMallon <penguin47@hotmail.co.uk>
2022-01-24 16:35:08 +0000

    basket functionality given

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def basket_contents(request):
 
@@ -7,11 +9,24 @@ def basket_contents(request):
     total = 0
     product_count = 0
     min_order = settings.MINIMUM_ORDER
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if product_count >= min_order:
         checkout = True
     else:
         checkout = False
+
+    delta = min_order - product_count
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = settings.DELIVERY_COST
@@ -21,9 +36,11 @@ def basket_contents(request):
     grand_total = total + delivery
 
     context = {
-        'basket_items': basket_items ,
+        'basket_items': basket_items,
         'product_count': product_count,
+        'delta': delta,
         'delivery': delivery,
+        'total': total,
         'grand_total': grand_total,
         'checkout': checkout,
     }
--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
-from products.models import Category
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from products.models import Product, Category
 
 # Create your views here.
 
@@ -30,3 +32,43 @@ def add_to_basket(request, item_id):
     request.session['basket'] = basket
     print(request.session['basket'])
     return redirect(redirect_url)
+
+def adjust_basket(request, item_id):
+    """ Adjust the quantity of the specified product in the shopping basket """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    
+    basket = request.session.get('basket', {}) # gets the basket variable if it exists, creates it if it doesn't.
+
+    if quantity > 0:
+        basket[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
+    else:
+        basket.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your basket')
+
+
+    request.session['basket'] = basket
+    
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """ Remove the specified product in the shopping basket """
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        
+        basket = request.session.get('basket', {}) # gets the basket variable if it exists, creates it if it doesn't.
+        
+        basket.pop(item_id)
+        
+        messages.success(request, f'Removed {product.name} from your basket')
+
+        request.session['basket'] = basket
+        
+        return HttpResponse(status=200)
+    
+    except Exception as e:
+        messages.error(request, f'Error removing item: (e)')
+        return HttpResponse(status=500)
