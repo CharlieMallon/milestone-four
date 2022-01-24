38f884309742464642b3c93b35e713e13d24bbdc
CharlieMallon <penguin47@hotmail.co.uk>
2022-01-24 19:49:42 +0000

    checkout forms, sginals and admin updated

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    # This is from the boutique_ado project
+    def ready(self):
+        import checkout.signals
