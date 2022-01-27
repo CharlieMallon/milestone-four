ccfd4e460366ede6ddbc6bbcd531f030c81768cd
CharlieMallon <penguin47@hotmail.co.uk>
2022-01-27 18:13:13 +0000

    refactored home app

--- home/views.py
+++ home/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
-from products.models import Category
 
 # Create your views here.
 
 def index(request):
-    """ A view to render the index page with the categories on it """
-
-    context = {
-
-    }
-
+    """ A view to render the index page """
     
-    return render(request, 'home/index.html', context)
+    return render(request, 'home/index.html')
