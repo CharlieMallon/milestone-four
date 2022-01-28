8af26391235cc6c758f0de1f41e708228447bd3a
CharlieMallon <penguin47@hotmail.co.uk>
2022-01-28 23:09:26 +0000

    initial commit of admin page

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     """ A view to render the index page """
     
     return render(request, 'home/index.html')
+
+
+def manage_shop(request):
+    """ A view to render the shop manager """
+    
+    return render(request, 'home/manage.html')
