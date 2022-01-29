0511eab3197c42e3acc34bd4f746beeb47d32616
CharlieMallon <penguin47@hotmail.co.uk>
2022-01-29 11:46:37 +0000

    functionality given to edit category

--- products/urls.py
+++ products/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
     path('categories/', views.categories, name='categories'),
     path('categories/add', views.add_category, name='add_category'),
-    # path('categories/edit/<int:category_id>/', views.edit_category, name='edit_category'),
+    path('categories/edit/<int:category_id>/', views.edit_category, name='edit_category'),
     # path('categories/delete/<int:category_id>/', views.delete_category, name='delete_category'),
 
 
