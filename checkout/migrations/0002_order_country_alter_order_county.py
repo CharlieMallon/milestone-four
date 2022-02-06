bf989820f720250a03c3a45a15b01ce1d573f3e0
CharlieMallon <penguin47@hotmail.co.uk>
2022-02-06 12:01:17 +0000

    started deployment

--- checkout/migrations/0002_order_country_alter_order_county.py
+++ checkout/migrations/0002_order_country_alter_order_county.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='order',
             name='country',
-            field=models.CharField(default=django.utils.timezone.now, max_length=20),
+            field=models.CharField(default=django.utils.timezone.now, max_length=50),
             preserve_default=False,
         ),
         migrations.AlterField(
