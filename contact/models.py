8ebc118b9ea3cd5606b280980792b3f37cf38a28
CharlieMallon <penguin47@hotmail.co.uk>
2022-02-08 22:21:23 +0000

    styled contact form

--- contact/models.py
+++ contact/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Contact(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    subject = models.TextField(null=True, blank=True, editable=True)
+    subject = models.CharField(max_length=254, null=True, blank=True, editable=True)
     message_text = models.TextField(null=True, blank=True, editable=True)
     date = models.DateTimeField(auto_now_add=True)
 
