241650bfe15dd826030d2acba55813512416a817
CharlieMallon <penguin47@hotmail.co.uk>
2022-02-09 15:20:12 +0000

    added basket update messages

--- basket/views.py
+++ basket/views.py
@@ -23,14 +23,17 @@ def add_to_basket(request, item_id):
     Add a quantity of the specified product to the shopping basket
     """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
     else:
         basket[item_id] = quantity
+        messages.success(request, f'Added {basket[item_id]} {product.name} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
