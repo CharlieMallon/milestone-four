f774effad86c37752d8b99b2fdf59dfb1a6f4d7b
CharlieMallon <penguin47@hotmail.co.uk>
2022-03-11 14:26:29 +0000

    PEP8 compliance

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,17 @@
+# pylint: disable=missing-module-docstring
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    """
+    set static storage location
+    """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """
+    set media storage location
+    """
     location = settings.MEDIAFILES_LOCATION
--- setting.py
+++ setting.py
@@ -1,6 +1,7 @@
+# pylint: disable=missing-module-docstring
 from dotenv import load_dotenv 
 load_dotenv()
 
 import os 
 SECRET_KEY = os.environ.get("SECRET_KEY") 
-DEBUG = "DEVELOPMENT" in os.environ 
+DEBUG = "DEVELOPMENT" in os.environ
