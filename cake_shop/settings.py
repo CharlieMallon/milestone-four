b1dab904c8b1dcdb7c635105098f6a1985749c07
CharlieMallon <penguin47@hotmail.co.uk>
2022-03-11 12:17:43 +0000

    PEP8 compliance - put dependancies back in

--- cake_shop/settings.py
+++ cake_shop/settings.py
@@ -10,9 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
+from re import L
 
 import os
 import dj_database_url
+
+if os.path.exists("env.py"):
+    import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
